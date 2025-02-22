24d4a2edc3250fa70333686ef92bdabb76c6f51b
AExpositoo <a.exposito.cainzos@udc.es>
2025-02-22 14:39:30 +0100

    fix: arregla el codigo para cargar los modelos una sola vez

--- classify.py
+++ classify.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
-from read_log import read_chat, save_chat, create_user
+from read_log import create_user, save_chat, read_chat
 
 def load_classifier():
     classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
@@ -26,33 +26,38 @@ def load_chatbot():
     return pipe
 
 def initialize_chatbot():
-    chat = [{"role": "system", "content": "Eres un asistente empático que se adapta según los sentimientos del usuario, los cuales serán especificados al final de cada mensaje junto a un nivel de confianaza para cada uno, pero no son parte del mensaje, solo una ayuda para que puedas contestarlo bien. Debes contestar en español."}]
+    chat = [{"role": "system", "content": "You are an empathetic assistant who adapts according to the user's feelings, which will be specified at the end of each message along with a confidence level for each. However, they are not part of the message, just a guide to help you respond appropriately."}]
     return chat
 
-def chatbot(pipe, text, chat):
-    classifier = load_classifier()
+def chatbot(pipe, text, chat, classifier):
     actual_emotions = get_emotion(classifier, text)
-    emotions_text = ", ".join([f"{emotion} (confianza: {score:.2f})" for emotion, score in actual_emotions.items()])
-    message = {"role": "user", "content": text + f"\nEmociones: {emotions_text}"}
+    emotions_text = ", ".join([f"{emotion} (confidence: {score:.2f})" for emotion, score in actual_emotions.items()])
+    message = {"role": "user", "content": text + f"\nEmotions: {emotions_text}"}
  
     chat.append(message)
+    response = pipe(chat, max_new_tokens=512)[0]['generated_text'][-1]['content']
+    response = response.split('</think>')[-1]
+    chat.append({'role': 'assistant', 'content': response})
  
-    return pipe(chat, max_tokens=512)
+    return chat
  
-def main_prueba(user):
+def load_models():
+    classifier = load_classifier()
     pipe = load_chatbot()
+    return classifier, pipe
 
+def main_prueba(user, pipe, classifier):
     if read_chat(user) == []:
         chat = initialize_chatbot()
     
     else:
         chat = read_chat(user)
-
     q = ""
     while q != "exit":
         q = str(input(">>>"))
-        chat = chatbot(pipe, q, chat)[0]['generated_text']
-        print(chat[-1]['content'])
+        chat = chatbot(pipe, q, chat, classifier)
+        # print(chat[-1]['content'])
+        print(chat)
     
     save_chat(user, chat)
 
