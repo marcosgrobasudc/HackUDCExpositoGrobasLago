732c68a17d44b84f5e31ca10d7be39df608e6b37
irenelagooo <i.lago@udc.es>
2025-02-23 08:27:45 +0100

    docs: realiza docstring de main.py

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ from classify import load_classifier
 from registry import cargar_registro, guardar_registro, generar_fechas_validas
 
 
-# Función para iniciar la interfaz
 def iniciar_interfaz(name):
+    """
+    Función para iniciar la interfaz con el nombre de usuario
+
+    Args:
+    name (str): nombre de usuario
+
+    Returns:
+    str: sal
+    """
     global username_value
     username_value = name
     saludo_actualizado = f"""
@@ -15,19 +23,25 @@ def iniciar_interfaz(name):
         Hola, {name}
     </div>
     """
-    return gr.update(visible=False), gr.update(visible=False), gr.update(value=saludo_actualizado), gr.Tabs(visible=True)
-
-
-
+    return gr.update(visible=False), gr.update(visible=False), gr.update(visible=False), gr.update(value=saludo_actualizado), gr.update(visible=True)
 
 def chatbot_wrapper(messages, history=None):
+    """
+    Wrapper para el chatbot que recibe los mensajes y llama a la función principal
+    
+    Args:
+    messages (list): lista de mensajes
+    history (list): historial de mensajes
+
+    Returns:
+    str: respuesta del chatbot
+    """
     print(username_value)
-    # print(username_input_value)
     return chatbot(username_value, pipe, messages, classifier)
 
 
 if __name__ == "__main__":
-    
+
     # Crear la interfaz con Gradio
     with gr.Blocks(title="KeleaCare") as app:
         gr.HTML("""<script> document.title = "KeleaCare"; </script>""")
@@ -36,15 +50,16 @@ if __name__ == "__main__":
         gr.HTML("""<style> body { overflow: hidden; height: 100vh; margin: 0; padding: 0; } .tabs-container { margin-top: -40px; }</style>""")
 
         # Componentes de inicio
-        imagen = gr.Image(value="https://media-asgard.s3.eu-west-1.amazonaws.com/22/09/15/20cc2074-3d1b-461c-8442-ddff0d14ca2d_kelea.png", 
-                  label="", show_label=False, interactive=False,
-                  elem_id="imagen_kelea", image_mode="auto", height=100)
-        username_input = gr.Textbox(label="Introduce tu nombre de usuario", placeholder="username")
-        # username_input_value=f"Hola {username_input}"
-        iniciar_btn = gr.Button("Iniciar")
+        with gr.Column():  # Columna para centrar los elementos verticalmente al principio
+            imagen = gr.Image(value="https://media-asgard.s3.eu-west-1.amazonaws.com/22/09/15/20cc2074-3d1b-461c-8442-ddff0d14ca2d_kelea.png", 
+                              label="", show_label=False, interactive=False,
+                              elem_id="imagen_kelea", image_mode="auto", height=100)  # Imagen inicial centrada
+            username_input = gr.Textbox(label="Introduce tu nombre de usuario", placeholder="username")
+            iniciar_btn = gr.Button("Iniciar")
 
-        # Saludo personalizado (inicialmente oculto)
-        greeting = gr.HTML("")  
+        # Saludo personalizado (inicialmente oculto) y logo pequeño
+        greeting = gr.HTML("")
+
 
         # Pestañas de opciones (inicialmente ocultas)
         tabs = gr.Tabs(visible=False, elem_classes=["tabs-container"])
@@ -69,20 +84,18 @@ if __name__ == "__main__":
                 selected_date.change(cargar_registro, inputs=[selected_date, username_input], outputs=entry)
                 guardar_btn.click(guardar_registro, inputs=[selected_date, entry, username_input], outputs=output_registro)
             
+
             with gr.TabItem("Realizar test de BigFive"):
                 analizar_btn = gr.Button("Realizar Análisis Big Five")
                 output_test = gr.Markdown()  # Cambiado a Markdown para aceptar formato
                 output_container = gr.Textbox(
                     label="Resultados del test",
-                    lines=9,  # Altura de 400px aprox.
-                    max_lines=9,
                     interactive=False,
-                    show_label=True
                 )
-                analizar_btn.click(fn=analise_big5, inputs=[username_input], outputs=output_test)
+                analizar_btn.click(fn=analise_big5, inputs=[username_input], outputs=output_container)
 
         # Botón de inicio
-        iniciar_btn.click(iniciar_interfaz, inputs=username_input, outputs=[username_input, iniciar_btn, greeting, tabs])
+        iniciar_btn.click(iniciar_interfaz, inputs=username_input, outputs=[username_input, iniciar_btn, imagen, greeting, tabs])
 
     # Lanzar la app
     app.launch(share=True)
